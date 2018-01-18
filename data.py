5dcf657f513181f61aa45b5fb0d5cc86e8d6d0b3
sian-nadin <nadin.sian@gmail.com>
2018-01-18 14:58:07 +0000

    Feat:Generate csv files

---
+++ data.py
@@ -0,0 +1,184 @@
+# -*- coding: utf-8 -*-
+
+import csv
+import codecs
+import pprint
+import re
+import xml.etree.cElementTree as ET
+
+import cerberus
+
+import schema
+
+
+from audit import update_street_name
+from eircodes import update_eircode
+
+FILENAME = "dublin_ireland.osm"
+
+
+NODES_PATH = "nodes.csv"
+NODE_TAGS_PATH = "nodes_tags.csv"
+WAYS_PATH = "ways.csv"
+WAY_NODES_PATH = "ways_nodes.csv"
+WAY_TAGS_PATH = "ways_tags.csv"
+
+LOWER_COLON = re.compile(r'^([a-z]|_)+:([a-z]|_)+')
+PROBLEMCHARS = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
+
+SCHEMA = schema.schema
+
+# Make sure the fields order in the csvs matches the column order in the sql table schema
+NODE_FIELDS = ['id', 'lat', 'lon', 'user', 'uid', 'version', 'changeset', 'timestamp']
+NODE_TAGS_FIELDS = ['id', 'key', 'value', 'type']
+WAY_FIELDS = ['id', 'user', 'uid', 'version', 'changeset', 'timestamp']
+WAY_TAGS_FIELDS = ['id', 'key', 'value', 'type']
+WAY_NODES_FIELDS = ['id', 'node_id', 'position']
+
+
+def shape_element(element, node_attr_fields=NODE_FIELDS, way_attr_fields=WAY_FIELDS,
+                  problem_chars=PROBLEMCHARS, default_tag_type='regular'):
+    """Clean and shape node or way XML element to Python dict"""
+
+    node_attribs = {}
+    way_attribs = {}
+    way_nodes = []
+    tags = []  # Handle secondary tags the same way for both node and way elements
+
+    # TODO: transform each element into the correct format
+    if element.tag == 'node':
+        for field in NODE_FIELDS:
+            # node_attribs contains top level node attributes
+            node_attribs[field] = element.attrib[field]
+
+        for node in element:
+            if node.tag == 'nd':
+                way_dict = {}
+                way_dict['id'] = element.attrib['id']
+                way_dict['node_id'] = node.attrib['ref']
+                way_dict['position'] = n
+                n += 1
+                way_nodes.append(way_dict)
+
+            if node.tag == 'tag':
+                tag_dict = {}
+                tag_dict['id'] = element.attrib['id']
+                if ':' in node.attrib['k']:
+                    tag_dict['type'] = node.attrib['k'].split(':', 1)[0]
+                    tag_dict['key'] = node.attrib['k'].split(':', 1)[-1]
+                    tag_dict['value'] = node.attrib['v']
+                else:
+                    tag_dict['type'] = 'regular'
+                    tag_dict['key'] = node.attrib['k']
+                    tag_dict['value'] = node.attrib['v']
+                tags.append(tag_dict)
+
+            node_attribs[field] = element.attrib[field]
+        return {'node': node_attribs, 'node_tags': tags}
+
+    elif element.tag == 'way':
+        for field in WAY_FIELDS:
+            way_attribs[field] = element.attrib[field]
+        n=0
+
+        for node in element:
+            if node.tag == 'nd':
+                way_dict = {}
+                way_dict['id'] = element.attrib['id']
+                way_dict['node_id'] = node.attrib['ref']
+                way_dict['position'] = n
+                n += 1
+                way_nodes.append(way_dict)
+
+            elif node.tag == 'tag':
+                tag_dict = {}
+                tag_dict['id'] = element.attrib['id']
+                tag_dict['value'] = node.attrib['v']
+                if ':' in node.attrib['k']:
+                    tag_dict['type'] = node.attrib['k'].split(':', 1)[0]
+                    tag_dict['key'] = node.attrib['k'].split(':', 1)[-1]
+                else:
+                    tag_dict['type'] = 'regular'
+                    tag_dict['key'] = node.attrib['k']
+                tags.append(tag_dict)
+        return {'way': way_attribs, 'way_nodes': way_nodes, 'way_tags': tags}
+
+
+# ================================================== #
+#               Helper Functions                     #
+# ================================================== #
+def get_element(osm_file, tags=('node', 'way', 'relation')):
+    """Yield element if it is the right type of tag"""
+
+    context = ET.iterparse(osm_file, events=('start', 'end'))
+    _, root = next(context)
+    for event, elem in context:
+        if event == 'end' and elem.tag in tags:
+            yield elem
+            root.clear()
+
+
+def validate_element(element, validator, schema=SCHEMA):
+    """Raise ValidationError if element does not match schema"""
+    if validator.validate(element, schema) is not True:
+        field, errors = next(validator.errors.iteritems())
+        message_string = "\nElement of type '{0}' has the following errors:\n{1}"
+        error_string = pprint.pformat(errors)
+
+        raise Exception(message_string.format(field, error_string))
+
+
+class UnicodeDictWriter(csv.DictWriter, object):
+    """Extend csv.DictWriter to handle Unicode input"""
+
+    def writerow(self, row):
+        super(UnicodeDictWriter, self).writerow({
+            k: (v.encode('utf-8') if isinstance(v, unicode) else v) for k, v in row.iteritems()
+        })
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
+
+
+# ================================================== #
+#               Main Function                        #
+# ================================================== #
+def process_map(file_in, validate):
+    """Iteratively process each XML element and write to csv(s)"""
+
+    with codecs.open(NODES_PATH, 'w') as nodes_file, \
+         codecs.open(NODE_TAGS_PATH, 'w') as nodes_tags_file, \
+         codecs.open(WAYS_PATH, 'w') as ways_file, \
+         codecs.open(WAY_NODES_PATH, 'w') as way_nodes_file, \
+         codecs.open(WAY_TAGS_PATH, 'w') as way_tags_file:
+
+        nodes_writer = UnicodeDictWriter(nodes_file, NODE_FIELDS)
+        node_tags_writer = UnicodeDictWriter(nodes_tags_file, NODE_TAGS_FIELDS)
+        ways_writer = UnicodeDictWriter(ways_file, WAY_FIELDS)
+        way_nodes_writer = UnicodeDictWriter(way_nodes_file, WAY_NODES_FIELDS)
+        way_tags_writer = UnicodeDictWriter(way_tags_file, WAY_TAGS_FIELDS)
+
+        nodes_writer.writeheader()
+        node_tags_writer.writeheader()
+        ways_writer.writeheader()
+        way_nodes_writer.writeheader()
+        way_tags_writer.writeheader()
+
+        validator = cerberus.Validator()
+
+        for element in get_element(file_in, tags=('node', 'way')):
+            el = shape_element(element)
+            if el:
+                if validate is True:
+                    validate_element(el, validator)
+
+                if element.tag == 'node':
+                    nodes_writer.writerow(el['node'])
+                    node_tags_writer.writerows(el['node_tags'])
+                elif element.tag == 'way':
+                    ways_writer.writerow(el['way'])
+                    way_nodes_writer.writerows(el['way_nodes'])
+                    way_tags_writer.writerows(el['way_tags'])
+
+process_map(FILENAME, validate=True)
