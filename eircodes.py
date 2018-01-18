8ebba25af7553870c61e14a68fbc64135901fb13
sian-nadin <nadin.sian@gmail.com>
2018-01-18 10:32:52 +0000

    Feat: Fix formatting for Eircodes

--- eircodes.py
+++ eircodes.py
@@ -8,11 +8,6 @@ def is_eircode(elem):
     return (elem.attrib['k'] == "addr:postcode")
 
 def audit_eircode(eircodes,value):
-    m = eircode_re.search(value)
-    if m:
-        eircode = m.group()
-        eircodes[eircode].add(value)
-    value = value.upper()
     eircodes[value].add(value)
     return eircodes
 
@@ -26,3 +21,22 @@ def audit(osmfile):
                     eircodes = audit_eircode(eircodes,tag.attrib['v'])
     osm_file.close()
     return eircodes
+
+mapping = { "D01X2P2": "D01 X2P2",
+            "D02X285": "D02 X285",
+            "D05N7F2": "D05 N7F2",
+            "D08P 89W":"D08 P89W",
+            "D09VY19":"D09 VY19",
+            "D15KPW7":"D15 KPW7",
+            "D6WXK28":"D6W XK28",
+            "d09 f6x0":"D09 F6X0"
+            }
+
+def update_eircode(osmfile):
+    eircodes = audit(osmfile)
+    for eircode, ways in eircodes.items():
+        for name in ways:
+            if eircode in mapping:
+                new_name = name.replace(eircode, mapping[eircode])
+                print(name, "=>", new_name)
+    return name
