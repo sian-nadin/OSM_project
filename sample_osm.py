f2cb3ad9d50b57552e59e6eb1854fc768b082c50
sian-nadin <nadin.sian@gmail.com>
2018-01-20 14:04:59 +0000

    Create a smaller sample file for testing on

--- sample_osm.py
+++ sample_osm.py
@@ -6,7 +6,7 @@ import xml.etree.ElementTree as ET  # Use cElementTree or lxml if too slow
 OSM_FILE = "dublin_ireland.osm"  # Replace this with your osm file
 SAMPLE_FILE = "sample.osm"
 
-k = 5 # Parameter: take every k-th top level element
+k = 35 # Parameter: take every k-th top level element
 
 def get_element(osm_file, tags=('node', 'way', 'relation')):
     """Yield element if it is the right type of tag
